b987448716bc75bd37e60ee6e19c490edff9ffd2
propstg <g@prop.st>
2024-08-09 23:36:56 -0400

    PlateUp 1.2.0 compatibility

--- OvercookedPatience/Main.cs
+++ OvercookedPatience/Main.cs
@@ -10,10 +10,10 @@ namespace KitchenOvercookedPatience {
 
         public const string MOD_ID = "blargle.overcookedpatience";
         public const string MOD_NAME = "Overcooked Patience";
-        public const string MOD_VERSION = "0.6.4";
+        public const string MOD_VERSION = "0.6.5";
         public const float MOD_VERSION_AS_FLOAT = 0.6f;
 
-        public Mod() : base(MOD_ID, MOD_NAME, "blargle", MOD_VERSION, ">=1.1.7", Assembly.GetExecutingAssembly()) { }
+        public Mod() : base(MOD_ID, MOD_NAME, "blargle", MOD_VERSION, ">=1.2.0", Assembly.GetExecutingAssembly()) { }
 
         protected override void OnInitialise() {
             Debug.Log($"[{MOD_ID}] v{MOD_VERSION} initialized");
@@ -23,9 +23,9 @@ namespace KitchenOvercookedPatience {
         }
 
         private void initPauseMenu() {
-            ModsPreferencesMenu<PauseMenuAction>.RegisterMenu(MOD_NAME, typeof(OvercookedPatienceMenu<PauseMenuAction>), typeof(PauseMenuAction));
-            Events.PreferenceMenu_PauseMenu_CreateSubmenusEvent += (s, args) => {
-                args.Menus.Add(typeof(OvercookedPatienceMenu<PauseMenuAction>), new OvercookedPatienceMenu<PauseMenuAction>(args.Container, args.Module_list));
+            ModsPreferencesMenu<MenuAction>.RegisterMenu(MOD_NAME, typeof(OvercookedPatienceMenu<MenuAction>), typeof(MenuAction));
+            Events.PlayerPauseView_SetupMenusEvent += (s, args) => {
+                args.addMenu.Invoke(args.instance, new object[] { typeof(OvercookedPatienceMenu<MenuAction>), new OvercookedPatienceMenu<MenuAction>(args.instance.ButtonContainer, args.module_list) } );
             };
         }
 
