c87d605e6ae07638f8feb9b8ba80d9d3f6e02b5a
propstg <g@prop.st>
2025-09-25 01:00:34 -0400

    update for PlateUp! 1.3.0

--- OvercookedPatience/Main.cs
+++ OvercookedPatience/Main.cs
@@ -10,7 +10,7 @@ namespace KitchenOvercookedPatience {
 
         public const string MOD_ID = "blargle.overcookedpatience";
         public const string MOD_NAME = "Overcooked Patience";
-        public const string MOD_VERSION = "0.6.6";
+        public const string MOD_VERSION = "0.6.7";
         public const float MOD_VERSION_AS_FLOAT = 0.6f;
 
         public Mod() : base(MOD_ID, MOD_NAME, "blargle", MOD_VERSION, ">=1.2.0", Assembly.GetExecutingAssembly()) { }
